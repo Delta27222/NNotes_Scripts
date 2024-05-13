6ff75cca51a40dd8abc9418f2c57485106dc8f96
italovisconti <hermanosvisconti@hotmail.com>
2024-05-13 07:55:54 -0400

    back, lb, and mongo sh added

---
+++ back.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# estas variables pueden ser exportadas en el sh padre con export <VARIABLE>
+replicaSet_name="uniRS"
+mongo_base_name="dbc"
+# y podemos hacer lo mismo para los ciclo for, porque podemos crear mas o menos de 3 contenedores
+back_base_name="backc"
+
+echo "running back.sh"
+#armamos el uri de mongo usando dbc.lxd
+mongo_uri="mongodb://"
+
+for i in $(seq 1 3)
+do
+  if [ "$i" -lt 3 ]
+  then
+    #mongo_uri+="${mongo_base_name}:2888,"
+    mongo_uri=$(printf "%s%s%s.lxd:2888," "$mongo_uri" "$mongo_base_name" "$i")
+  else
+    #mongo_uri+="${mongo_base_name}:2888"
+    mongo_uri=$(printf "%s%s%s.lxd:2888/" "$mongo_uri" "$mongo_base_name" "$i")
+  fi
+done
+
+#mongo_uri+="/?replicaSet=${replicaSet_name}"
+mongo_uri=$(printf "%s?replicaSet=%s" "$mongo_uri" "$replicaSet_name")
+echo "mongo URI: ${mongo_uri}"
+
+for i in $(seq 1 3)
+do
+  container_name="${back_base_name}${i}"
+  sudo lxc launch ubuntu:jammy "$container_name" --config cloud-init.user-data="$(cat back.yaml)"
+
+  # debemos buscar las ip de las bases de datos podemos usar lxc list, o creamos un txt en el sh de mongo, y accedemos a ese txt
+  # tambien podemos aprovechar el dns de los contenedores
+  echo "corriendo express app en ${container_name}"
+  sudo lxc exec "$container_name" -- bash -c "cd /root/myapp && echo PORT=3000 > .env && echo DB_URI=$mongo_uri >> .env && nohup npx ts-node src/app.ts &"
+#podemos daemonizar la corrida del back creando un nuevo servicio
+#[Unit]
+#Description=My App
+
+#[Service]
+#ExecStart= algo con npx 
+#ExecStart=/usr/local/bin/npm run dev
+#Restart=always
+#User=nobody
+#Environment=PATH=/usr/bin:/usr/local/bin
+#Environment=NODE_ENV=production
+#WorkingDirectory=/path/to/your/app
+
+#[Install]
+#WantedBy=multi-user.target
+
+
+#sudo systemctl daemon-reload
+#sudo systemctl start my-app
+done
---
+++ mongo.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+base_name="dbc"
+
+# <comando> > /dev/null 2>&1 sirve para que el stderr y stdout se vayan a /dev/null y no se muestra nada en consola
+
+#corremos un comando para pingear la bd que debe estar corriendo en el puerto indicado
+#y chequeamos $? que es una variable que guarda el exit status del ultimo comando que se corrio
+#0 quiere decir que el comando fue exitoso, y cualquier no-0 quiere decir que el comando no fue exitoso
+check_mongo_running() {
+  local container_name="$1"
+  local port="$2"
+  local mongo_command="mongosh --port $port --eval 'db.runCommand({ ping: 1})'"
+  sudo lxc exec "$container_name" -- mongosh --port $port --eval 'db.runCommand({ping:1})' > /dev/null 2>&1
+  return $?
+}
+#obtenemos la lista de contenedores, y agarramos la segunda cadena de la linea con el contenedor que buscamos
+get_container_ip() {
+  local container_name="$1"
+  sudo lxc list | grep "$container_name" | awk '{print $6}'
+}
+
+echo "running mongo.sh"
+
+for i in $(seq 1 3)
+do
+  container_name="${base_name}${i}"
+  #echo "Launching c: $container_name"
+  sudo lxc launch ubuntu:jammy "$container_name" --config cloud-init.user-data="$(cat mongoRS.yaml)"
+done
+
+#obtenemos todas las ip de los contendores creados
+#y hacemos rs.initiate() junto a rs.add()
+for i in $(seq 1 3)
+do
+  primary_name="${base_name}1"
+  container_name="${base_name}${i}"
+  container_ip=$(get_container_ip "$container_name")
+
+  echo "checking mongo in $container_name - $container_ip"
+
+  until check_mongo_running "$container_name" 2888
+  do
+    echo "Waiting for mongo to start in $container_name"
+    sleep 6
+  done
+
+  if [ $i -eq 1 ]
+  then
+    echo "initiating rs"
+    sudo lxc exec "${primary_name}" -- mongosh --port 2888 --eval 'rs.initiate()'
+  else
+    echo "adding ${container_name} to rs"
+    sudo lxc exec "${primary_name}" -- mongosh --port 2888 --eval "rs.add(\"${container_ip}:2888\")"
+  fi
+done
+
+#tengo que retornar las ip de los contenedores listos
+echo "mongo done!"
+
+#hay que armar un json y es problematico, es mejor iniciar el rs y agregar 1 a 1
+#sudo lxc exec "${base_name}1" -- mongosh --port 2888 --eval 'rs.initiate({_id: "uniRS", members: [{ _id: 0, host: "${container_ip}:2888" }, { _id: 1, host: "${container_ip}:2888"}]})'
+
